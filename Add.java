a17a84f7646481f08dde5be018cec8776c314c8f
TT63T <teesathomas3@gmail.com>
2025-08-11 22:24:16 +0530

    package

---
+++ Add.java
@@ -0,0 +1,8 @@
+package repo;
+public class Add{
+public void addition()
+{
+int a=100; 
+int b=200;
+System.out.println("the sum is" +(a+b));
+}}
---
+++ Tee.java
@@ -0,0 +1,8 @@
+import repo.Add;
+class Tee
+{
+public static void main(String args[])
+{
+Add obj=new Add();
+obj.addition();
+}}
